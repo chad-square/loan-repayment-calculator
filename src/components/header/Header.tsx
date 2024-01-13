9448b5044e0723d351725d383b4a179b8c5192f9
chad <chad.square1@gmail.com>
2024-01-13 10:56:57 +0200

    added memo to Header component to prevent unnecessary re-rendering

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,12 @@
 import classes from "./Header.module.scss";
+import {memo} from "react";
 
-export default function Header() {
+const Header = memo(function Header() {
     return (
         <header className={classes.header}>
             <h2>Loan Repayment Calculator</h2>
         </header>
     )
-}
+})
+
+export default Header;
